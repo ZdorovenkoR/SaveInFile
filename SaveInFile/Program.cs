1be42ad9c082d4d9b10e3f3a96675337a5077f80
ZdorovenkoRoman <ZdorovenkoR@gmail.com>
2020-12-30 00:50:06 +0600

    Добавьте файлы проекта.

---
+++ SaveInFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+
+namespace SaveInFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
