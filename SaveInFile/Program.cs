1a36d4292baeb00b7617282c1bc86266246bf76f
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2020-12-30 00:51:43 +0600

    Update Program.cs

--- SaveInFile/Program.cs
+++ SaveInFile/Program.cs
@@ -12,7 +12,12 @@ namespace SaveInFile
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Введите данные");
+            string userData = Console.ReadLine();
+            StreamWriter userData2 = new StreamWriter("test.txt", true); ;
+            userData2.WriteLine(userData);
+            userData2.Close();
+            Console.WriteLine("Данные сохранены");
         }
     }
 }
