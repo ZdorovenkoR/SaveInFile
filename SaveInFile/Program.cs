c9d34f1bbdd7d38830fae835fa822d8249a1401f
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2020-12-30 01:20:12 +0600

    Update Program.cs

--- SaveInFile/Program.cs
+++ SaveInFile/Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+//Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл..
 
 namespace SaveInFile
 {
